60b04ad2fe7888604c74fb53556591eb786be62e
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-04-17 13:23:00 +0900

    Update get_mapping.py

--- data/get_mapping.py
+++ data/get_mapping.py
@@ -1,1 +1,11 @@
+import os
+# root path for the BraTS2018 dataset
+data_path = '.'
 
+f = open(os.path.join(data_path, 'name_mapping.csv'), 'w+')
+f.writelines('Grade,BraTS_2018_subject_ID\n')
+for label in ['HGG', 'LGG']:
+    path = os.path.join(data_path, label)
+    for dir_name in os.listdir(path):
+        f.writelines(f'{label},{dir_name}\n')
+f.close()
