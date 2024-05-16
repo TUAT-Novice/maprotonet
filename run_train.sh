7dc28e71aeb83b2612a2d64b9a28f39c4aba2c7a
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-05-16 18:34:30 +0900

    Add files via upload

---
+++ run_train.sh
@@ -0,0 +1,27 @@
+# for ddp
+export nproc_per_node=1
+export nnodes=1
+export node_rank=0
+
+# dataset
+export data_path=D:/data/BraTS2018/MICCAI_BraTS_2018_Data_Training
+
+# model
+export model=cnn
+
+if [[ $model =~ maprotonet ]]
+  then
+    bash ./scripts/run_maprotonet.sh
+elif [[ $model =~ mprotonet ]]
+  then
+    bash ./scripts/run_mprotonet.sh
+elif [[ $model =~ xprotonet ]]
+  then
+    bash ./scripts/run_xprotonet.sh
+elif [[ $model =~ protopnet ]]
+  then
+    bash ./scripts/run_protopnet.sh
+elif [[ $model =~ cnn ]]
+  then
+    bash ./scripts/run_cnn.sh
+fi
