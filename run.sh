098a017093f4e73a0bed9115d26e1fb24f050a24
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-05-21 21:25:32 +0900

    Update and rename run_train.sh to run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ export data_path=D:/data/BraTS2018/MICCAI_BraTS_2018_Data_Training
 # model
 export model=maprotonet
 
-# provide the model hash code when you want to evaluate, such as maprotonet_eefb07f7
+# provide the model hash code only when you want to evaluate, such as maprotonet_eefb07f7
 # export load_model=hash_code_of_the_model
 
 if [[ $model =~ maprotonet ]]
