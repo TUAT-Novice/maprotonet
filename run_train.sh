42c154a299d9ebfe5a7213475cb4b124939ed24d
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-05-21 21:25:15 +0900

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -4,24 +4,27 @@ export nnodes=1
 export node_rank=0
 
 # dataset
-export data_path=path_to_name_mapping_file
+export data_path=D:/data/BraTS2018/MICCAI_BraTS_2018_Data_Training
 
 # model
-export model=cnn
+export model=maprotonet
+
+# provide the model hash code when you want to evaluate, such as maprotonet_eefb07f7
+# export load_model=hash_code_of_the_model
 
 if [[ $model =~ maprotonet ]]
   then
-    bash ./scripts/train_maprotonet.sh
+    bash ./scripts/run_maprotonet.sh
 elif [[ $model =~ mprotonet ]]
   then
-    bash ./scripts/train_mprotonet.sh
+  bash ./scripts/run_mprotonet.sh
 elif [[ $model =~ xprotonet ]]
   then
-    bash ./scripts/train_xprotonet.sh
+    bash ./scripts/run_xprotonet.sh
 elif [[ $model =~ protopnet ]]
   then
-    bash ./scripts/train_protopnet.sh
+    bash ./scripts/run_protopnet.sh
 elif [[ $model =~ cnn ]]
   then
-    bash ./scripts/train_cnn.sh
+    bash ./scripts/run_cnn.sh
 fi
