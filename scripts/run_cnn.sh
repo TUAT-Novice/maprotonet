812f88655da1a0aeb28078a33805dd5a9a54ae81
TUAT-Novice <164989847+TUAT-Novice@users.noreply.github.com>
2024-06-14 16:17:59 +0900

    Update run_cnn.sh

--- scripts/run_cnn.sh
+++ scripts/run_cnn.sh
@@ -12,7 +12,7 @@ n_res_block=2
 
 if  [ ! -n "$load_model" ]
   then # for train
-    python -m torch.distributed.launch --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank ./tumor_cls.py \
+    python ./tumor_cls.py \
       -d $data_path \
       -m $model \
       -n $epoch \
@@ -24,7 +24,7 @@ if  [ ! -n "$load_model" ]
       --save-model 1 \
       -s $seed
   else  # for eval
-    python -m torch.distributed.launch --nproc_per_node $nproc_per_node --nnodes $nnodes --node_rank $node_rank ./tumor_cls.py \
+    python ./tumor_cls.py \
       --load-model $load_model \
       -d $data_path \
       -m $model \
